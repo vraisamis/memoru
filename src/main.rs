781bd82cf8847c2a9234436fbc5d88d40863a396
vraisamis <quick.sorting.arg@gmail.com>
2021-09-17 03:21:36 +0900

    とりあえずclapをいれてnewコマンドを受け取れるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Clap)]
+enum Command {
+    New(New),
+}
+
+#[derive(Clap)]
+struct New {
+    #[clap(short, long)]
+    title: Option<String>,
+}
+
+impl New {
+    fn run(&self, opts: &Opts) {
+        if let Some(title) = &self.title {
+            println!("title is '{}'", title);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match &opts.command {
+        Command::New(new) => new.run(&opts),
+    }
 }
