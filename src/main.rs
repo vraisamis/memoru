625c4c59fac8bbd29d67b02d18e23b6fc8260366
vraisamis <quick.sorting.arg@gmail.com>
2021-09-21 22:04:54 +0900

    title受け取り

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 mod options;
 
+use std::sync::Mutex;
+
+use anyhow::Result;
 use clap::Clap;
+use once_cell::sync::Lazy;
+use rustyline::Editor;
 
 use options::{Command, Opts};
 
-fn main() {
+static READ_LINE: Lazy<Mutex<Editor<()>>> = Lazy::new(|| Mutex::new(Editor::new()));
+
+fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
 
     match &opts.command {
-        Command::New(new) => new.run(&opts),
-    }
+        Command::New(new) => {
+            new.run(&opts)?;
+        }
+    };
+    Ok(())
 }
--- src/options/new.rs
+++ src/options/new.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use clap::Clap;
 
 use super::Opts;
@@ -9,11 +10,13 @@ pub struct New {
 }
 
 impl New {
-    pub fn run(&self, _opts: &Opts) {
-        if let Some(title) = &self.title {
-            println!("title is '{}'.", title);
-        } else {
-            println!("title IS NOT SET.");
-        }
+    pub fn run(&self, _opts: &Opts) -> Result<()> {
+        let title: String = self.title.clone().unwrap_or_else(|| {
+            let mut r = crate::READ_LINE.lock().unwrap();
+            r.readline("Title is not set.\nType the title: ")
+                .unwrap_or_else(|_| "NO_TITLE".to_string())
+        });
+        println!("THE TITLE IS [{}]", title);
+        Ok(())
     }
 }
