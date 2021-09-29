1d9ecba7bc39c58fc10a8391671a4807ca4af296
vraisamis <quick.sorting.arg@gmail.com>
2021-09-29 21:13:16 +0900

    new実装

--- src/options/new.rs
+++ src/options/new.rs
@@ -1,4 +1,8 @@
+use std::fs::File;
+use std::io::Write;
+
 use anyhow::Result;
+use chrono::Local;
 use clap::Clap;
 
 use super::Opts;
@@ -13,10 +17,25 @@ impl New {
     pub fn run(&self, _opts: &Opts) -> Result<()> {
         let title: String = self.title.clone().unwrap_or_else(|| {
             let mut r = crate::READ_LINE.lock().unwrap();
-            r.readline("Title is not set.\nType the title: ")
-                .unwrap_or_else(|_| "NO_TITLE".to_string())
+            println!("Title is not set.");
+            r.readline("Type the title: ").map_or_else(
+                |_| "NO_TITLE".to_string(),
+                |s| {
+                    if s.len() > 0 {
+                        s
+                    } else {
+                        "NO_TITLE".to_string()
+                    }
+                },
+            )
         });
-        println!("THE TITLE IS [{}]", title);
+        dbg!(&title);
+        let local = Local::now().format("%Y-%m-%d").to_string();
+        dbg!(&local);
+
+        let filename = format!("{}-{}.md", local, title);
+        let mut file = File::create(filename)?;
+        file.write_all(format!("# {}", title).as_bytes())?;
         Ok(())
     }
 }
